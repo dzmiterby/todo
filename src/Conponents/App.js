9e9af78159ecb471f3551e45f27c4ac90b722240
Dzmiter <dzmiter2105@gmail.com>
2023-09-05 18:53:09 +0300

    ver4

--- src/Conponents/App.js
+++ src/Conponents/App.js
@@ -1,6 +1,7 @@
-/* eslint import/order: ["error", {"newlines-between": "always"}] */
+/* eslint import/order: ["error", {"newlines-between": "always-and-inside-groups"}] */
 import React from 'react';
 import './App.css';
+
 import { v4 as uuid } from 'uuid';
 import NewTaskForm from './NewTaskForm';
 import TaskList from './TaskList';

d80d9f637b7846f58bb96ac340a6b4290dce8314
Dzmiter <dzmiter2105@gmail.com>
2023-09-05 18:57:29 +0300

    ver5

--- src/Conponents/App.js
+++ src/Conponents/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import './App.css';
 
 import { v4 as uuid } from 'uuid';
+
 import NewTaskForm from './NewTaskForm';
 import TaskList from './TaskList';
 import Footer from './Footer';
